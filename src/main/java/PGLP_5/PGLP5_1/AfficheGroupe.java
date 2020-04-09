18efda1ef98e2ceb6a84e38f19941a85d98be3d5
KI Serge Laobien <sergelaobienk@gmail.com>
2020-04-09 11:11:58 +0000

    Mis a jour des classe Affichegrp compositepersonnels personnels

--- src/main/java/PGLP_5/PGLP5_1/AfficheGroupe.java
+++ src/main/java/PGLP_5/PGLP5_1/AfficheGroupe.java
@@ -1,5 +1,75 @@
 package PGLP_5.PGLP5_1;
 
-public class AfficheGroupe {
+import java.io.Serializable;
+
+public class AfficheGroupe implements Serializable{
+	/**
+	 * la version d’un classe pour la sérialisation 
+	 */
+//	private static final long serialVersionUID = 1L;
+	public ArrayList<InterfacePersonnels> grp=new ArrayList<InterfacePersonnels>();
+	
+	public AfficheGroupe (InterfacePersonnels root) {
+		this.grp.add(root);
+	}
+	
+	/**
+	 * 
+	 * @author root
+	 *
+	 */
+	private class groupIterator implements Iterator{
+		int index=0;
+		
+		
+		public groupIterator() {
+			int verifier=0;
+			while(verifier<grp.size()) {
+				if(grp.get(verifier) instanceof CompositePersonnels) {
+					CompositePersonnels test=(CompositePersonnels)grp.get(verifier);
+					int i=0;
+					
+					while(i<test.grpPerso.size()) {
+						grp.add(test.grpPerso.get(i));
+						
+						i++;
+					}
+					
+				
+				}
+				verifier++;
+			}
+		}
+		
+		/**
+		 * verifirie s'il y'a des elements dans l'arr
+		 */
+		@Override
+		public boolean HasNext() {
+			if(index<grp.size())
+				return true;
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public PersonelsInter Next() {
+			if(this.HasNext()) {
+				index++;
+				return grp.get(index-1);
+			}
+				
+			return null;
+		}
+		
+		
+		
+	}
+	
+	public Iterator getIterator() {
+		return new groupIterator();
+		
+	}
+
 
 }
